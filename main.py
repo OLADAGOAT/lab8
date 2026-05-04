0e2b8e373d4075d7567a87ba6b540ef1ec2fcaf8
olamipo adewumi <olamipo.adewumi@epita.fr>
2026-05-04 10:39:19 +0200

    exam: q3 - screen wrapping

--- main.py
+++ main.py
@@ -89,13 +89,16 @@ def update_squares(squares: List[Square], dt: float) -> None:
         square.rect.x += random.randint(-1, 1)
         square.rect.y += random.randint(-1, 1)
 
-        if square.rect.left < 0 or square.rect.right > SCREEN_WIDTH:
-            square.velocity = (-square.velocity[0], square.velocity[1])
-            square.rect.x = max(0, min(square.rect.x, SCREEN_WIDTH - square.rect.width))
+        # Screen wrapping instead of bouncing
+        if square.rect.left > SCREEN_WIDTH:
+            square.rect.right = 0
+        elif square.rect.right < 0:
+            square.rect.left = SCREEN_WIDTH
 
-        if square.rect.top < 0 or square.rect.bottom > SCREEN_HEIGHT:
-            square.velocity = (square.velocity[0], -square.velocity[1])
-            square.rect.y = max(0, min(square.rect.y, SCREEN_HEIGHT - square.rect.height))
+        if square.rect.top > SCREEN_HEIGHT:
+            square.rect.bottom = 0
+        elif square.rect.bottom < 0:
+            square.rect.top = SCREEN_HEIGHT
 
     for i in range(len(squares) - 1, -1, -1):
         if squares[i].age >= squares[i].life_span:
